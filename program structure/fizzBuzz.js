964fa4c7c7ec5539942cbce2006dbbb8ab5820b7
Leoloopy <abolajiidiisu@gmail.com>
2022-10-22 12:45:43 +0100

    fizz buzz

--- program structure/fizzBuzz.js
+++ program structure/fizzBuzz.js
@@ -1,1 +1,8 @@
-const 
+const divisibleByThree = 3;
+const divsibleByFive = 5;
+
+for (let i = 0; i < 100; i++) {
+  if (i % 3 == 0) console.log(`fizz -> ${i}`);
+  if (i % 5 == 0) console.log(`buzz -> ${i}`);
+  if (i % 5 == 0 && i % 3 == 0) console.log(`fizzbuzz -> ${i}`);
+}
